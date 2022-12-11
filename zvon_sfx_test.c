b6f1ddb79efd67b46e109b8abbeb2ee042d8e70f
Peter Sovietov <peter.sovietov@gmail.com>
2022-12-11 21:48:15 +0300

    new ver. of test song

--- zvon_sfx_test.c
+++ zvon_sfx_test.c
@@ -34,46 +34,46 @@ void play(int num_samples, double vol) {
     free(samples);
 }
 
-void song(double vol) {
-    double notes[][CHANNELS] = {
-        {78, 0, 87 * 1, 87 * 2},
-        {78, 0, 87 * 2, 0},
-        {0, 78, 87 * 1, 0},
-        {0, 0, 87 * 2, 0},
-        {78, 0, 87 * 1, 0},
-        {78, 0, 87 * 2, 0},
-        {0, 78, 87 * 1, 0},
-        {0, 0, 87 * 2, 0},
-        {78, 0, 87 * 1, 87 * 3},
-        {78, 0, 87 * 2, 0},
-        {0, 78, 87 * 1, 0},
-        {0, 0, 87 * 2, 0},
-        {78, 0, 87 * 1, 0},
-        {78, 0, 87 * 2, 0},
-        {0, 78, 87 * 1, 0},
-        {0, 0, 87 * 2, 0},
-        {78, 0, 87 * 2, 233},
-        {78, 0, 87 * 3, 0},
-        {0, 78, 87 * 2, 0},
-        {0, 0, 87 * 3, 0},
-        {78, 0, 87 * 2, 0},
-        {78, 0, 87 * 3, 0},
-        {0, 78, 87 * 2, 0},
-        {0, 0, 87 * 3, 0},
-        {78, 0, 87 * 2, 233 * 2},
-        {78, 0, 87 * 3, 0},
-        {0, 78, 87 * 2, 0},
-        {0, 0, 87 * 3, 0},
-        {78, 0, 87 * 2, 0},
-        {78, 0, 87 * 3, 0},
-        {0, 78, 87 * 2, 0},
-        {0, 0, 87 * 3, 0}
+double song[][CHANNELS] = {
+    {78, 0, 87 * 1, 87 * 2},
+    {78, 0, 87 * 2, 0},
+    {0, 78, 87 * 1, 0},
+    {0, 0, 87 * 2, 0},
+    {78, 0, 87 * 1, 0},
+    {78, 0, 87 * 2, 0},
+    {0, 78, 87 * 1, 0},
+    {0, 0, 87 * 2, 0},
+    {78, 0, 87 * 1, 87 * 3},
+    {78, 0, 87 * 2, 0},
+    {0, 78, 87 * 1, 0},
+    {0, 0, 87 * 2, 0},
+    {78, 0, 87 * 1, 0},
+    {78, 0, 87 * 2, 0},
+    {0, 78, 87 * 1, 0},
+    {0, 0, 87 * 2, 0},
+    {78, 0, 87 * 2, 233},
+    {78, 0, 87 * 3, 0},
+    {0, 78, 87 * 2, 0},
+    {0, 0, 87 * 3, 0},
+    {78, 0, 87 * 2, 0},
+    {78, 0, 87 * 3, 0},
+    {0, 78, 87 * 2, 0},
+    {0, 0, 87 * 3, 0},
+    {78, 0, 87 * 2, 233 * 2},
+    {78, 0, 87 * 3, 0},
+    {0, 78, 87 * 2, 0},
+    {0, 0, 87 * 3, 0},
+    {78, 0, 87 * 2, 0},
+    {78, 0, 87 * 3, 0},
+    {0, 78, 87 * 2, 0},
+    {0, 0, 87 * 3, 0}
+};
 
-    };
-    for (int i = 0; i < (int) (sizeof(notes) / sizeof(notes[0])); i++) {
-        for (int j = 0; j < CHANNELS; j++) {
-            if (notes[i][j]) {
-                zv_note_on(j, notes[i][j]);
+void play_song(double song[][CHANNELS], int song_size, int num_chans, double vol) {
+    for (int i = 0; i < song_size; i++) {
+        for (int j = 0; j < num_chans; j++) {
+            if (song[i][j]) {
+                zv_note_on(j, song[i][j]);
             }
         }
         play(sec(0.2), vol);
@@ -94,7 +94,7 @@ void bass_drum_preset(struct sfx_box *box) {
     sfx_box_change(box, ZV_LFO_LOOP, 0, 0);
     sfx_box_change(box, ZV_LFO_ASSIGN, 1, LFO_TARGET_WIDTH);
     sfx_box_change(box, ZV_LFO_FUNC, 1, LFO_SAW);
-    sfx_box_change(box, ZV_LFO_FREQ, 1, 15);
+    sfx_box_change(box, ZV_LFO_FREQ, 1, 10);
     sfx_box_change(box, ZV_LFO_LOW, 1, 0.9);
     sfx_box_change(box, ZV_LFO_HIGH, 1, 0.1);
     sfx_box_change(box, ZV_LFO_LOOP, 1, 0);
@@ -105,13 +105,13 @@ void snare_drum_preset(struct sfx_box *box) {
     sfx_box_change(box, ZV_AMP, 0, 10000);
     sfx_box_change(box, ZV_WIDTH, 0, 10000);
     sfx_box_change(box, ZV_OFFSET, 0, 0);
-    sfx_box_change(box, ZV_DECAY, 0, 0.13);
+    sfx_box_change(box, ZV_DECAY, 0, 0.15);
     sfx_box_change(box, ZV_SUSTAIN, 0, 0);
     sfx_box_change(box, ZV_RELEASE, 0, 0);
     sfx_box_change(box, ZV_LFO_ASSIGN, 0, LFO_TARGET_FREQ);
     sfx_box_change(box, ZV_LFO_FUNC, 0, LFO_SAW);
-    sfx_box_change(box, ZV_LFO_FREQ, 0, 5);
-    sfx_box_change(box, ZV_LFO_LOW, 0, 120);
+    sfx_box_change(box, ZV_LFO_FREQ, 0, 10);
+    sfx_box_change(box, ZV_LFO_LOW, 0, 150);
     sfx_box_change(box, ZV_LFO_HIGH, 0, 0);
     sfx_box_change(box, ZV_LFO_LOOP, 0, 0);
     sfx_box_change(box, ZV_LFO_ASSIGN, 1, LFO_TARGET_OFFSET);
@@ -132,7 +132,7 @@ void bass_synth_preset(struct sfx_box *box) {
     sfx_box_change(box, ZV_LFO_FREQ, 0, 5);
     sfx_box_change(box, ZV_LFO_LOW, 0, 0.8);
     sfx_box_change(box, ZV_LFO_HIGH, 0, 0.3);
-    sfx_box_change(box, ZV_LFO_LOOP, 0, 0);
+    sfx_box_change(box, ZV_LFO_LOOP, 0, 1);
 }
 
 double semi(double x) {
@@ -141,7 +141,7 @@ double semi(double x) {
 
 void lead_synth_preset(struct sfx_box *box) {
     sfx_box_change(box, ZV_MODE, 0, OSC_PWM);
-    sfx_box_change(box, ZV_WIDTH, 0, 0.75);
+    sfx_box_change(box, ZV_WIDTH, 0, 0);
     sfx_box_change(box, ZV_OFFSET, 0, 1.7);
     sfx_box_change(box, ZV_DECAY, 0, 0.5);
     sfx_box_change(box, ZV_SUSTAIN, 0, 1);
@@ -153,17 +153,20 @@ void lead_synth_preset(struct sfx_box *box) {
     sfx_box_change(box, ZV_LFO_LOW, 0, 0);
     sfx_box_change(box, ZV_LFO_HIGH, 0, 1);
     sfx_box_change(box, ZV_LFO_LOOP, 0, 1);
-
     sfx_box_change(box, ZV_LFO_SEQ_POS, 0, 0);
     sfx_box_change(box, ZV_LFO_SEQ_VAL, 0, semi(0));
-
     sfx_box_change(box, ZV_LFO_SEQ_POS, 0, 1);
     sfx_box_change(box, ZV_LFO_SEQ_VAL, 0, semi(7));
-
     sfx_box_change(box, ZV_LFO_SEQ_POS, 0, 2);
     sfx_box_change(box, ZV_LFO_SEQ_VAL, 0, semi(12));
-
     sfx_box_change(box, ZV_LFO_SEQ_SIZE, 0, 3);
+
+    sfx_box_change(box, ZV_LFO_ASSIGN, 1, LFO_TARGET_WIDTH);
+    sfx_box_change(box, ZV_LFO_FUNC, 1, LFO_SAW);
+    sfx_box_change(box, ZV_LFO_FREQ, 1, 10);
+    sfx_box_change(box, ZV_LFO_LOW, 1, 0.75);
+    sfx_box_change(box, ZV_LFO_HIGH, 1, 0);
+    sfx_box_change(box, ZV_LFO_LOOP, 1, 1);
 }
 
 int main(int argc, char **argv) {
@@ -184,10 +187,10 @@ int main(int argc, char **argv) {
     chan_set_pan(&channels[3], 0);
     struct sfx_box *box1 = chan_push(&channels[0], &sfx_synth);
     bass_drum_preset(box1);
-    sfx_box_change(box1, ZV_VOLUME, 0, 0.24);
+    sfx_box_change(box1, ZV_VOLUME, 0, 0.25);
     struct sfx_box *box2 = chan_push(&channels[1], &sfx_synth);
     snare_drum_preset(box2);
-    sfx_box_change(box2, ZV_VOLUME, 0, 0.3);
+    sfx_box_change(box2, ZV_VOLUME, 0, 0.25);
     struct sfx_box *box3 = chan_push(&channels[2], &sfx_synth);
     bass_synth_preset(box3);
     sfx_box_change(box3, ZV_VOLUME, 0, 0.2);
@@ -195,8 +198,12 @@ int main(int argc, char **argv) {
     lead_synth_preset(box4);
     struct sfx_box *box5 = chan_push(&channels[3], &sfx_delay);
     sfx_box_change(box5, ZV_TIME, 0, 0.2);
-    sfx_box_change(box4, ZV_VOLUME, 0, 0.05);
-    song(0.8);
+    sfx_box_change(box4, ZV_VOLUME, 0, 0.1);
+    play_song(song, (sizeof(song) / sizeof(song[0])), 2, 0.7);
+    play_song(song, (sizeof(song) / sizeof(song[0])) / 2, 3, 0.7);
+    play_song(song, (sizeof(song) / sizeof(song[0])) / 2, 3, 0.7);
+    play_song(song, (sizeof(song) / sizeof(song[0])), 4, 0.7);
+    play_song(song, (sizeof(song) / sizeof(song[0])), 4, 0.7);
     for (int i = 0; i < CHANNELS; i++) {
         chan_drop(&channels[i]);
     }
