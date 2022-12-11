aab8b7766b719decad6f7069cb1dbfcb11b9e86b
Peter Sovietov <peter.sovietov@gmail.com>
2022-12-11 12:34:34 +0300

    cleanup

--- raw2wav.bat
+++
@@ -1,1 +0,0 @@
-"D:\tools\sox-14.4.2\sox.exe" -r 44100 -e floating-point -b 32 -c 2 sfx_test.raw sfx_test.wav
--- test.bat
+++
@@ -1,3 +0,0 @@
-gcc -std=c99 -Wall -Wextra -pedantic -O3 -g zvon.c zvon_mixer.c zvon_sfx.c zvon_sfx_test.c -o zvon_sfx_test.exe
-zvon_sfx_test.exe
-raw2wav.bat
